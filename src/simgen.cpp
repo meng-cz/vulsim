8e9938963322a8185f71dba8b7c03e66663bb079
meng-cz <mengcz1126@gmail.com>
2026-04-20 07:29:14 +0000

    fix: user test header include name mismatch

--- src/simgen.cpp
+++ src/simgen.cpp
@@ -1636,7 +1636,7 @@ ErrorMsg genTestHarnessHpp(const VulTestHarnessModule &test_module, const VulMod
     out_lines.push_back("#include \"config.h\"\n");
     out_lines.push_back("#include \"bundle.h\"\n");
     out_lines.push_back("#include \"vullib.h\"\n");
-    out_lines.push_back("#include \"testheader.h\"\n");
+    out_lines.push_back("#include \"testheader.hpp\"\n");
     out_lines.push_back("\n");
 
     out_lines.push_back("#include \"" + top_module.name + ".hpp\"\n");
